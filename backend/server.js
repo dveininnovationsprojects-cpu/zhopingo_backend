54605ca3debdf85a00db757e4034ffa183eb833c
dveininnovationsprojects-cpu <dveininnovations.projects@gmail.com>
2026-02-06 18:21:28 +0530

    updated mongodb

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cors({
 
 const apiRoutes = require('./routes/apiRoutes');
 // --- 🌟 STATIC FILES ---
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads/kyc', express.static(path.join(__dirname, 'uploads/kyc')));
 
 // --- 🌟 ROUTES ---
