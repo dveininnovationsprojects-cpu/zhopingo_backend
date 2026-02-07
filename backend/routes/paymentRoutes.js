480848125d1093401237ddd3569dddeb5d78611b
dveininnovationsprojects-cpu <dveininnovations.projects@gmail.com>
2026-02-07 10:45:24 +0530

    updated mongodb

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 
 router.post("/create-session", paymentController.createPaymentSession);
-router.post("/cashfree/webhook", paymentController.cashfreeWebhook);
 router.get("/verify/:orderId", paymentController.verifyPayment);
 
 module.exports = router;
