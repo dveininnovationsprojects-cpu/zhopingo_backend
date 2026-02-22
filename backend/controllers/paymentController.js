f2dfab1edfb145223f43569d3b21900253f14068
dveininnovationsprojects-cpu <dveininnovations.projects@gmail.com>
2026-02-22 10:07:03 +0530

    update mongodb

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -262,7 +262,7 @@ exports.createSession = async (req, res) => {
     const request = StandardCheckoutPayRequest.builder()
       .merchantOrderId(order._id.toString())
       .amount(Math.round(order.totalAmount * 100))
-      .redirectUrl(`${process.env.BASE_URL}/api/v1/payments/phonepe-return/${orderId}`)
+      .redirectUrl(`https://api.zhopingo.in/api/v1/payments/phonepe-return/${orderId}`)
       .build();
 
     const response = await client.pay(request);
